d8e6fcd7befad28ce7141e0a100b80eb9d335379
agry88 <rgok307085@gmail.com>
2023-11-10 22:17:39 +0800

    docs: add success response type to swagger.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,32 +10,54 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { UserDto } from './dto/user-dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The user has been successfully created.',
+    type: UserDto,
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @ApiOkResponse({
+    description: 'The users has been successfully found.',
+    type: [UserDto],
+  })
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({
+    description: 'The user has been successfully found.',
+    type: UserDto,
+  })
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({
+    description: 'The user has been successfully updated.',
+    type: UserDto,
+  })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({
+    description: 'The user has been successfully removed.',
+    type: UserDto,
+  })
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
   }
